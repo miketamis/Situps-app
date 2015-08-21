5f30306e9aa2b1c4ad0fbaec0d680aba09a83130
Mike Tamis <mike@navicomdynamics.com>
2015-08-21 15:12:38 +1200

    Fixed up WorkoutGenerator so it outputs more reasonable outcomes and checks for bad inputs

--- app/scripts/utils/WorkoutGenerator.js
+++ app/scripts/utils/WorkoutGenerator.js
@@ -1,15 +1,18 @@
 'use strict';
+var assert = require('assert');
 
 function baseNumber(n, age) {
-    return Math.ceil(Math.pow(n * (age + 1) / 4, 2));
+    return Math.ceil(Math.pow(n * (5 - age) / 4, 2));
 }
 
 var restAmount = 60;
 
 var WorkoutGenerator = {
     generateWorkout: function(n, age) {
+        assert(age === 0 || age === 1 || age === 2, 'Invalid Age');
+        assert(n > 0 && Number.isInteger(n), 'Invalid Level');
         var base = baseNumber(n, age);
-        var w1 = (n % 2) + 1;
+        var w1 = (n % 2) + n;
         var w2 = (n % 3) + (n % 2);
         var w3 = ((n + 1) % 2);
         return [
@@ -23,7 +26,9 @@ var WorkoutGenerator = {
         ];
     },
     calculateLevel: function(maxReps, age) {
-        return Math.sqrt(maxReps / 1.5) * 4 / (age + 1);
+        assert(maxReps > 0 && Number.isInteger(maxReps), 'Invalid Amount');
+        assert(age === 0 || age === 1 || age === 2, 'Invalid Age');
+        return Math.ceil(Math.sqrt(maxReps / 4) * 4 / (5 - age)) ;
     }
 };
 
