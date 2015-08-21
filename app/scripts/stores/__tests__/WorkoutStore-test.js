7db2285e04c0573b3943df2fd400bc75e770aca7
Mike Tamis <mike@navicomdynamics.com>
2015-08-21 16:35:29 +1200

    added some more basic Store tests.

--- app/scripts/stores/__tests__/WorkoutStore-test.js
+++ app/scripts/stores/__tests__/WorkoutStore-test.js
@@ -6,14 +6,25 @@ describe('TodoStore', function() {
     var AppDispatcher;
     var WorkoutStore;
     var callback;
+    var listener;
 
     beforeEach(function() {
         AppDispatcher = require('../../dispatcher/AppDispatcher');
         WorkoutStore = require('../WorkoutStore');
         callback = AppDispatcher.register.mock.calls[0][0];
+        listener = jest.genMockFunction();
+        WorkoutStore.addChangeListener(listener);
+    });
+
+    afterEach(function() {
+        WorkoutStore.removeChangeListener(listener);
     });
 
     it('registers a callback with the dispatcher', function() {
         expect(AppDispatcher.register.mock.calls.length).toBe(1);
+    });
+
+    it('it doesn\'t crash when a random action is dispatch', function() {
+        callback({awesome: 'is so awesome'});
     });
 });
