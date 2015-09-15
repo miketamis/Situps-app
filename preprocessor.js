ac58282ed9fa9dd446fbc9161a2b4c1eab244367
Mike Tamis <tamis.mike@gmail.com>
2015-09-16 10:43:07 +1200

    Made preprocessing faster

--- preprocessor.js
+++ preprocessor.js
@@ -1,8 +1,8 @@
 var ReactTools = require('react-tools');
 module.exports = {
-    process: function(src) {
-        return ReactTools.transform(src, {
-            harmony: true
-        });
+    process: function(src, file) {
+        // We really only care about JSX and React test files
+        if (/\.jsx$/.test(file) || /react-test.js$/.test(file)) return ReactTools.transform(src);
+        return src;
     }
 };
