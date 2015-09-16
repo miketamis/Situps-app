f85bdebd9ddd782aade84288970bd4b7bcf7aff0
Mike Tamis <tamis.mike@gmail.com>
2015-09-17 08:23:28 +1200

    Wrote Hint test

--- app/scripts/components/__tests__/Hint.react-test.js
+++ app/scripts/components/__tests__/Hint.react-test.js
@@ -3,8 +3,26 @@ jest.dontMock('classnames');
 
 describe('Hint', function () {
     var React = require('react/addons');
-    var TestUtils = React.addons.TestUtils;
-    it('TODO', function() {
+
+    it('should render', function() {
         var Hint = require('../Hint.react.jsx');
+        var html = React.renderToStaticMarkup(
+            < Hint value='next' position='top' className='Hello'/>
+        );
+        expect(html).toBe('<div class="Hello hint top">next</div>');
+    });
+
+    it('should fail if value isnt assigned', function() {
+        var Hint = require('../Hint.react.jsx');
+        expect(function() {
+            React.renderToStaticMarkup(< Hint position='bottom'/>);
+        }).toThrow('Value must be defined');
+    });
+
+    it('should fail if possition isnt valid', function() {
+        var Hint = require('../Hint.react.jsx');
+        expect(function() {
+            React.renderToStaticMarkup(< Hint value='MY HINT' position = 'awesome'/>);
+        }).toThrow('position must be one of top, middle or bottom');
     });
 });
