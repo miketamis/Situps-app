45ea67594751be16ae149158bcaada0edcc6984b
Mike Tamis <tamis.mike@gmail.com>
2015-09-17 08:23:48 +1200

    Wrote MainMenu Test

--- app/scripts/components/__tests__/MainMenu.react-test.js
+++ app/scripts/components/__tests__/MainMenu.react-test.js
@@ -4,7 +4,21 @@ jest.dontMock('classnames');
 describe('MainMenu', function () {
     var React = require('react/addons');
     var TestUtils = React.addons.TestUtils;
-    it('TODO', function() {
+    it('It goes back', function() {
         var MainMenu = require('../MainMenu.react.jsx');
+        var standardActions = require('../../actions/standardActions');
+        var DOM = TestUtils.renderIntoDocument(<MainMenu/>);
+        DOM.watchListener({data:'back-button:click'});
+        expect(standardActions.goBack).toBeCalled();
+        DOM.componentWillUnmount();
+    });
+
+    it('On select changes the page', function() {
+        var MainMenu = require('../MainMenu.react.jsx');
+        var standardActions = require('../../actions/standardActions');
+        var DOM = TestUtils.renderIntoDocument(<MainMenu/>);
+        DOM.onSelect({text: 'Hello'});
+        expect(standardActions.optionSelected).toBeCalledWith('Hello');
+        DOM.componentWillUnmount();
     });
 });
