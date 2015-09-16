6f80de1bb9cbc6a211d398a3d40cfcec38f134d5
Mike Tamis <tamis.mike@gmail.com>
2015-09-16 12:29:22 +1200

    Wrote test for CircleNumber

--- app/scripts/components/CircleNumber.react.jsx
+++ app/scripts/components/CircleNumber.react.jsx
@@ -2,8 +2,13 @@
 'use strict';
 
 var React = require('react');
+var assert = require('assert');
 
 var MainMenu = React.createClass({
+    getInitialState: function() {
+        assert(this.props.value, 'Value must be defined');
+        return null;
+    },
   /**
    * @return {object}
    */
--- app/scripts/components/__tests__/CircleNumber.react-test.js
+++ app/scripts/components/__tests__/CircleNumber.react-test.js
@@ -3,8 +3,16 @@ jest.dontMock('classnames');
 
 describe('CircleNumber', function () {
     var React = require('react/addons');
-    var TestUtils = React.addons.TestUtils;
-    it('TODO', function() {
+    it('should render', function() {
         var CircleNumber = require('../CircleNumber.react.jsx');
+        var html = React.renderToStaticMarkup(< CircleNumber value={4}/>);
+        expect(html).toBe('<div class="circle">4</div>');
+    });
+    
+    it('should fail if value is assigned', function() {
+        var CircleNumber = require('../CircleNumber.react.jsx');
+        expect(function() {
+            React.renderToStaticMarkup(< CircleNumber />);
+        }).toThrow('Value must be defined');
     });
 });
