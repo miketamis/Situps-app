3c889473d9982e1477b645713e5d88c956a04450
Mike Tamis <tamis.mike@gmail.com>
2015-09-17 08:22:15 +1200

    Added asserts to Hint

--- app/scripts/components/Hint.react.jsx
+++ app/scripts/components/Hint.react.jsx
@@ -2,12 +2,17 @@
 'use strict';
 
 var React = require('react');
+var assert = require('assert');
 
 var SitupScreen = React.createClass({
     /**
     * @return {object}
     */
     render: function() {
+        assert(['top', 'middle', 'bottom'].indexOf(this.props.position) != -1,
+         'position must be one of top, middle or bottom');
+        assert(this.props.value, 'Value must be defined');
+
         return (
             <div className={ this.props.className + ' hint ' + this.props.position }>
                 {this.props.value}
