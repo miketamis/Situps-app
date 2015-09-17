c11986b5101f522f8b4232ccbbf31a8d7b8dce81
Mike Tamis <tamis.mike@gmail.com>
2015-09-18 10:35:10 +1200

    fixed app.react skeleton

--- app/scripts/__tests__/app.react-test.js
+++ app/scripts/__tests__/app.react-test.js
@@ -1,10 +1,11 @@
 jest.dontMock('../app.jsx');
 jest.dontMock('classnames');
+jest.dontMock('object-assign');
 
-describe('WorkoutScreen', function () {
+describe('App', function () {
     var React = require('react/addons');
     var TestUtils = React.addons.TestUtils;
     it('TODO', function() {
-        var WorkoutScreen = require('../app.jsx');
+        //var app = require('../app.jsx');
     });
 });
