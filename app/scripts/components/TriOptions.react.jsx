938dc5b78f802101b3ac3b98eb0e597861ccb3d0
Mike Tamis <tamis.mike@gmail.com>
2015-09-18 16:39:59 +1200

    Added asserts to TriOptions

--- app/scripts/components/TriOptions.react.jsx
+++ app/scripts/components/TriOptions.react.jsx
@@ -3,14 +3,17 @@
 
 var React = require('react');
 var Hint = require('./Hint.react.jsx');
-
-//var Arrow = String.fromCharCode(9664);
+var assert = require('assert');
 
 var TriOptions = React.createClass({
     /**
     * @return {object}
     */
     render: function() {
+        assert(this.props.optionOne, 'optionOne must be defined');
+        assert(this.props.optionTwo, 'optionTwo must be defined');
+        assert(this.props.optionThree, 'optionThree must be defined');
+        
         return (<div>
             <Hint className="tri-option" position="top" value={this.props.optionOne}/>
             <Hint className="tri-option" position="middle" value={this.props.optionTwo}/>
